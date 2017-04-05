717c01651b72c208c89ee37108f93b5b5252f3a3
Black Spirit <panagiotis_11@hotmail.com>
2017-04-05 22:32:16 +0000

    Completed server API Examples: * https://.../1491350400 => {"unix":1491350400,"natural":"April 5, 2017"} * https://.../April%205,%202017 => {"unix":1491350400,"natural":"April 5, 2017"} * https://.../NotADate => {"unix":null,"natural":null}

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var moment = require('moment');
 var app = express();
 var port = 8080;
 
@@ -7,9 +8,20 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:timestamp', function(req, res) {
-  var dateUnix = new Date(parseInt(req.params.timestamp) * 1000);
-  var dateStr = new Date(req.params.timestamp);
-  console.log(dateStr);
+  var obj = { unix: null, natural: null };
+  var format = 'MMMM D, YYYY';
+  var timestamp = req.params.timestamp;
+  var unix = moment.unix(timestamp);
+  var date = moment(timestamp, format);
+  if (unix.isValid()) {
+    obj.unix = unix.unix();
+    obj.natural = unix.format(format);
+  } else if (date.isValid()) {
+    obj.unix = date.unix();
+    obj.natural = date.format(format);
+  }
+  
+  res.send(obj);
 });
 
 app.listen(port, function () {
